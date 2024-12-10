d7510d881106d503ddaa41c0cc3a5d83c1f04e3c
sheakeogh <shealmk@gmail.com>
2024-12-10 15:58:39 +0000

    Answer for Product of Array except self

---
+++ src/ArrayString/ProductOfArrayExceptSelf.java
@@ -0,0 +1,32 @@
+package ArrayString;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] prefixProduct = new int[nums.length];
+        int[] suffixProduct = new int[nums.length];
+        int[] answer = new int[nums.length];
+
+        prefixProduct[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            prefixProduct[i] = prefixProduct[i - 1] * nums[i - 1];
+        }
+
+        suffixProduct[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            suffixProduct[i] = suffixProduct[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = prefixProduct[i] * suffixProduct[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(productExceptSelf(new int[] {-1, 1, 0, -3, 3})));
+    }
+}
